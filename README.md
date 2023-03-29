90e599083338ccd485d2d490afccde8b07b36917
nvaccina <niccolovac@gmail.com>
2023-03-29 15:35:33 +0200

    Aggiunta select e classi larghezza box

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Campominato
 L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.<br>
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emette un messaggio in console con il numero della cella cliccata.<br>
 **Bonus**<br>
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
@@ -16,3 +16,5 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - Al click del bottone il container diventa display:block e il bottone display:none;
 - Genero il box con la classe "box";
 - Al click del box aggiungo la classe clicked e visualizzo in cosole l'id con il numero rispettivo al box cliccato;
+- Aggiungo una select per i 3 diversi livelli di difficoltà;
+- Creo 3 classi differenti con 3 larghezze dei box diverse, che poi andrò ad aggiungere al box a seconda della selezione fatta nella select; 
--- js/script.js
+++ js/script.js
@@ -1,40 +1,41 @@
-//HTML ELEMENTS
+//ELEMENTS
 const play = document.querySelector('.play-button');
 const container = document.querySelector('.container');
 let counterBox = 0;
 
+//aggiungo la classe hide al container
 container.classList.add('hide');
+console.log(container);
 
 //AZIONE PLAY
 play.addEventListener('click', function(){
-
-  container.classList.remove('hide');
+  //al click il button play prende la classe hide e il container la rimuove
   play.classList.add('hide');
-  console.log(container, play);
-
+  container.classList.remove('hide');
 
   for(let i = 0; i < 100; i++){
     const currentbox = createBox();
     container.appendChild(currentbox);
+    //inserisco le condizioni a seconda dei livelli
 
     //aggiungo al box l'id con un numero progressivo da 1 a 100
     currentbox.boxId = i + 1;
 
-    //al click del box, il box si colora
+    //Al click del box, il box si colora e restituisce il numero id in console
     currentbox.addEventListener('click',function(){
+      //ali click del box assegno la classe 'clicked'
       this.classList.toggle('clicked');
-
-      //
+      //assegno l'id alla variabile counterBox
       counterBox = this.boxId;
+      //al click di un box, visualizzo in console il numero corrispondente tramite l'id
       console.log(this.boxId);
     })
   }
 })
 
-
 /*----------------------FUNCTION------------------------*/
 //genero il box
-function createBox() {
+function createBox(){
   const newBox = document.createElement('div');
   newBox.className = 'box';  
   return newBox;
